373fbaa52884a9fff9bcfa4fbb27e2f7ab1ad9b3
NH <34393417+InnerQi@users.noreply.github.com>
2023-05-09 10:00:50 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# Simple band name generator #
+# Asks user for city they grew up in and name of pet
+# Program generators a band name based on the inputs given.
+
+print("Welcome to the band name generator.")
+city = input("What city did you grow up in?\n")   # saves input into a variable
+pet = input("What is the name of your pet?\n")    # saves input into a variable
+print("Your band name is " + city + " " + pet)    # Use string concatenation
+
+
